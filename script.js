d7a5e158d09067dbd2bdb302addada6e9cf34fe8
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-10-14 21:52:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const parallelogramWidthMax = 320;
 const parallelogramWidthMin = 192;
+const rowHeight = 48;
+const speedX = 24;
+const speedY = 14.997;
+const rowCount = Math.ceil(window.innerHeight +  heightOffset(window.innerWidth))/ rowHeight);
 
 function randomNumber(min, max) {
   return Math.random() * (max-min) + min);
 }
 
+function heightOffset(x) {
+    let angle = 32 * (Math.PI/180);
+    let y = x * Math.tan(angle);
+    return y;
+}
+
 class Parallelogram {
-  constructor() {
-    this.height = 32;
-    this.width = randomNumber(parallelogramWidthMin, parallelogramWidthMax);
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
   }
 }
 
