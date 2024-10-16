c1151b124875df32a5a5085faec05c54b8a8641a
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-10-16 06:48:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const parallelogramWidthMax = 320;
 const parallelogramWidthMin = 192;
 const parallelogramColors = ['hsl(317 100% 54% / .7)', 'hsl(197 100% 54% / .7)'];
 const parallelogramHeight = 32;
+const parallelogramCount = 20;
 const rowHeight = 48;
 const minimumSeperation = 32;
 const moveAngle = 32;
@@ -45,7 +46,7 @@ function createParalelogram(color, width, height, left, top) {
 }
 
 class Parallelogram {
-  constructor(row) {
+  constructor(row, firstSpawn) {
     this.width = randomNumber(parallelogramWidthMin, parallelogramWidthMax);
     this.height = parallelogramHeight;
     this.row = row;
@@ -55,6 +56,25 @@ class Parallelogram {
     this.lastTimestamp = null;
     this.element = createParalelogram(this.color, this.width, this.height, this.xPos, this.yPos);
     this.move = true;
+
+    if (firstSpawn) {
+      let xAdd = Math.random() * ((this.width * 3) + window.innerWidth);
+      let yAdd = xAdd * speedY;
+
+      this.xPos += xAdd;
+      this.yPos += yAdd;
+
+      let arrayIndex = rowClears[this.row].indexOf(this);
+      if (this.xPos >= minimumSeperation) {
+        if (arrayIndex > -1) {
+          rowClears[this.row].splice(arrayIndex, 1);
+        }
+      } else {
+        if (arrayIndex < 0) {
+          rowClears[this.row].push(this);
+        }
+      }
+    }
 
     if (this.move) requestAnimationFrame(this.updatePosition.bind(this));
   }
@@ -81,7 +101,7 @@ class Parallelogram {
       }
 
       if (this.xPos > window.innerWidth + this.width) {
-        spawnParallelogram();
+        spawnParallelogram(false);
         this.element.remove();
         this.move = false;
       }
@@ -98,7 +118,7 @@ class Parallelogram {
   }
 }
 
-function spawnParallelogram() {
+function spawnParallelogram(firstSpawn) {
   let spawnOptions = [];
   for (let i = 0; i < rowClears.length; i++) {
     if (rowClears[i].length === 0) {
@@ -107,7 +127,7 @@ function spawnParallelogram() {
   }
   const row = spawnOptions[Math.floor(Math.random() * spawnOptions.length)];
   if (row === undefined) throw new Error('row should not be undefined here');
-  new Parallelogram(row);
+  new Parallelogram(row, firstSpawn);
 }
 
 function initiateRowClears() {
@@ -117,6 +137,8 @@ function initiateRowClears() {
 }
 initiateRowClears();
 
-for (let i = 0; i < rowCount; i++) {
-  new Parallelogram(i);
+for (let i = 0; i < parallelogramCount; i++) {
+  spawnParallelogram(true);
 }
+
+//Add first spawn overlap protection
